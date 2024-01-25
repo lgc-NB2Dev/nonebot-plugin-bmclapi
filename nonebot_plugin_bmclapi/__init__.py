cd81e6bb5f5fbb7ba5322651b6d07c33a0ab6811
XieXiLin <support@xiexilin.com>
2024-01-25 19:42:57 +0800

    init

--- nonebot_plugin_bmclapi/__init__.py
+++ nonebot_plugin_bmclapi/__init__.py
@@ -8,11 +8,11 @@ from .config import ConfigModel  # noqa: E402
 __version__ = "0.1.0"
 __plugin_meta__ = PluginMetadata(
     name="nonebot-plugin-bmclapi",
-    description="插件模板",
-    usage="这是一个一个一个插件模板",
+    description="在聊天软件内插件 OpenBMCLAPI 状态",
+    usage="",
     type="application",
     homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-bmclapi",
     config=ConfigModel,
     supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
-    extra={"License": "MIT", "Author": "student_2333"},
+    extra={"License": "MIT", "Author": "student_2333 & XieXiLin"},
 )
